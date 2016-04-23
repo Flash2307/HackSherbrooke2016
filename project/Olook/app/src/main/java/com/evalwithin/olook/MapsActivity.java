368afde06823cce5447a302414f07e143310e5fa
Emile Fugulin <emilefugulin@hotmail.com>
2016-04-23 14:16:32 -0400

    Add icon and delete unused images in panel

--- project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
+++ project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
@@ -94,15 +94,9 @@ public class MapsActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
