c88f7dd16b0a7715155d09f0e4b9895c5a737ac3
shakela <frise.serre@hotmail.com>
2016-04-23 17:04:16 -0400

    Add options menu for filtering

--- project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
+++ project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
@@ -1,8 +1,10 @@
 package com.evalwithin.olook;
 
 import android.app.FragmentManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.app.Fragment;
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,9 +12,12 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
@@ -35,6 +40,7 @@ public class MapsActivity extends AppCompatActivity
 
     private GPSTracker tracker;
     private OrientationTracker orientationTracker;
+    private PopupMenu popupMenu;
 
     private static String TAG = MapsActivity.class.getSimpleName();
 
@@ -75,6 +81,29 @@ public class MapsActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        if(menu.size() == 0)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                menu.add(0, i, Menu.NONE, "item " + i).setCheckable(true);
+            }
+        }
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        menuItem.setChecked(!menuItem.isChecked());
+        return false;
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -84,12 +113,45 @@ public class MapsActivity extends AppCompatActivity
         }
     }
 
+    /*@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        if(menu.size() == 0)
+        {
+            menu.clear();
+
+            int id = 0;
+
+            for(int i = 0; i < 5; i++)
+            {
+                menu.add(0, id++, Menu.NONE, "item " + i).setCheckable(true);
+            }
+
+            getMenuInflater().inflate(R.menu.main, menu);
+        }
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        item.setChecked(!item.isChecked());
+
+        return true;
+    }
+
+    @Override
+    public void onOptionsMenuClosed(Menu menu)
+    {
+
+    }*/
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
@@ -122,21 +184,6 @@ public class MapsActivity extends AppCompatActivity
         CameraPosition pos = CameraPosition.builder(oldPos).bearing(245.0F)
                 .build();
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
