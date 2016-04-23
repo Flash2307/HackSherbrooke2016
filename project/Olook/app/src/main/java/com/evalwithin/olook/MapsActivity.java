91dc3c5edc16675639948702b4f1bd9bca7c703f
Emile Fugulin <emilefugulin@hotmail.com>
2016-04-23 14:00:32 -0400

    Minimal UI with bar

--- project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
+++ project/Olook/app/src/main/java/com/evalwithin/olook/MapsActivity.java
@@ -1,21 +1,14 @@
 package com.evalwithin.olook;
 
-import android.app.FragmentManager;
-import android.content.Context;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,104 +17,26 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
+public class MapsActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private static String TAG = MapsActivity.class.getSimpleName();
-
-    ListView m_drawerList;
-    RelativeLayout m_drawerPane;
-
-    private ActionBarDrawerToggle m_drawerToggle;
-    private DrawerLayout m_drawerLayout;
     private GoogleMap m_map;
-
-    ArrayList<NavigationItem> m_navigationItems = new ArrayList<NavigationItem>();
-
-    class NavigationItem {
-        String m_title;
-        String m_subTitle;
-        int m_Icon;
-
-        public NavigationItem(String title, String subtitle, int icon) {
-            m_title = title;
-            m_subTitle = subtitle;
-            m_Icon = icon;
-        }
-    }
-
-    class DrawerListAdapter extends BaseAdapter {
-
-        Context m_context;
-        ArrayList<NavigationItem> m_navigationItems;
-
-        public DrawerListAdapter(Context context, ArrayList<NavigationItem> navItems) {
-            m_context = context;
-            m_navigationItems = navItems;
-        }
-
-        @Override
-        public int getCount() {
-            return m_navigationItems.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return m_navigationItems.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view;
-
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) m_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.drawer_item, null);
-            }
-            else {
-                view = convertView;
-            }
-
-            TextView titleView = (TextView) view.findViewById(R.id.title);
-            TextView subtitleView = (TextView) view.findViewById(R.id.subTitle);
-            ImageView iconView = (ImageView) view.findViewById(R.id.icon);
-
-            titleView.setText( m_navigationItems.get(position).m_title );
-            subtitleView.setText( m_navigationItems.get(position).m_subTitle );
-            iconView.setImageResource(m_navigationItems.get(position).m_Icon);
-
-            return view;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        //Add options in sliding menu
-        m_navigationItems.add(new NavigationItem("Home", "Meetup destination", R.drawable.ic_action_home));
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        // Populate the Drawer with options
-        m_drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        m_drawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
-        m_drawerList = (ListView) findViewById(R.id.navList);
-        DrawerListAdapter adapter = new DrawerListAdapter(this, m_navigationItems);
-        m_drawerList.setAdapter(adapter);
-
-        // Click listeners for drawer
-        m_drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                selectItemFromDrawer(position);
-            }
-        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -129,16 +44,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         m_map = googleMap;
@@ -149,19 +71,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         m_map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    /*Called when item selected in drawer*/
-    private void selectItemFromDrawer(int position) {
-        Fragment fragment = new PreferencesFragment();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.mainContent, fragment)
-                .commit();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-        m_drawerList.setItemChecked(position, true);
-        setTitle(m_navigationItems.get(position).m_title);
+        return super.onOptionsItemSelected(item);
+    }
 
-        // Close the drawer
-        m_drawerLayout.closeDrawer(m_drawerPane);
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
